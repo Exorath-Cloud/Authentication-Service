f1dcdb78e751b2f15928981a5cd07653c189243c
Toon Sevrin <twan123@live.be>
2016-12-14 17:36:41 +0100

    Create AccessToken.java

---
+++ src/main/java/exorath/cloud/authentication/AccessToken.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Exorath
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+ 
+package exorath.cloud.authentication;
+
+/**
+ * Temporary session id for the browser, stored in cookies.
+ * Created by Toon Sevrin on 12/14/2016.
+ */
+public class AccessToken{
+  private String id;
+  private Date expiry;
+  
+  public String getId(){
+    return id;
+  }
+  public boolean isExpired(){
+    //if expiry date is in the past, return true
+  }
+  
+  public Date getExpiry(){
+    return expiry;
+  }
+}
