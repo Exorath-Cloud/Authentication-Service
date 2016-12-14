d9ccc8ef57f160aae4c7ddaf007930d9cc685025
Toon Sevrin <twan123@live.be>
2016-12-14 17:33:38 +0100

    Extended userdata

--- src/main/java/exorath/cloud/authentication/UserData.java
+++ src/main/java/exorath/cloud/authentication/UserData.java
@@ -4,9 +4,15 @@ package exorath.cloud.authentication;
  * Created by Connor on 12/14/2016.
  */
 public class UserData {
-
-    String username;
-    String password;
-    String accessToken;
-
+    private String username; //the unique identifier of the user
+    private String email; //email address of the user (will also be unique)
+    private String passwordHash; //the bscript hashed password with salt
+    
+    private boolean verified; //whether or not this user has verified his email address
+    private boolean enabled; //whether or not this account is enabled, an account can be disabled when it's banned
+    
+    private AccessToken accessToken; //session id for browsers, will regenerate after a certain timespan
+    
+    private Map<String, ResetToken> resetTokensById; //The password reset tokens by their id, they expire after a timespan and are single use.
+    
 }
